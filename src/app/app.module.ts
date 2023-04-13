5e198b2cc55f52b9ed09885b8c9c19f4580003a8
Joe <joe7663@gmail.com>
2023-04-13 16:58:41 -0400

    Component Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ManagerComponent } from './manager/manager.component';
+import { AdminComponent } from './admin/admin.component';
+import { LeaveRequestsComponent } from './leave-requests/leave-requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ManagerComponent,
+    AdminComponent,
+    LeaveRequestsComponent
   ],
   imports: [
     BrowserModule
