2ed8ab83ed7a1cbefaf8b740db13661ccfd46a1b
sadrachm <sadrachmart@gmail.com>
2023-04-13 18:07:37 -0700

    Can login to Employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,38 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginService } from './services/login.service';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
 const route: Routes = [
   {path:"", component:AppComponent},
-  {path:"login", component:LoginComponent}
+  {path:"login", component:LoginComponent},
+  {path:"home", component:HomeComponent}
 ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
+    RouterModule.forRoot(route),
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
     BrowserModule,
-    RouterModule.forRoot(route)
-  ],
-  exports: [
-    RouterModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule ,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  type:string = "admin";
+  username:string="";
+  password:string="";
+  constructor(private loginService:LoginService, private router:Router){}
+  toggleType() {
+    this.type = this.type=="admin" ? "employee" : "admin";
+  }
+  submit() {
+    if (this.type=="employee") {
+      this.loginService.login({"email":this.username, "password":this.password}).subscribe(data => {
+        console.log(data)
+        this.loginService.id = data.id as unknown as number;
+        this.router.navigate(['/home']);
+      })
+    } else if (this.type=="admin") {
+      console.log("We have not implemented an Admin Login feature yet")
+    } else {
+      console.log("How did you get here???")
+    }
+  }
+
+
 
 }
