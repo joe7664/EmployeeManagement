f434a4d990e030ab4075832b8889f38a9bf51ca0
Joe <joe7663@gmail.com>
2023-04-14 18:25:39 -0400

    Account editing done

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
+import { LoginService } from 'src/app/services/login.service';
 import { Employee } from 'src/app/models/Employee';
 
 @Component({
@@ -14,10 +15,14 @@ export class ProfileComponent {
     firstName: '',
     lastName: '',
     password: '',
-    phoneNumber: ''
+    phoneNumber: '',
+    id: 0
   };
-  constructor(private profile : ProfileService){}
+  constructor(private profile : ProfileService, private emp : LoginService){ }
   ngOnInit(): void{
-    this.profile.retrieveInfo().subscribe(json => this.employee = json);
+    this.profile.retrieveInfo(this.emp.id).subscribe(json => this.employee = json);
+  }
+  updateInfo(){
+    this.profile.updateInfo(this.employee).subscribe(json => this.employee = json);
   }
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -2,24 +2,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Employee } from '../models/Employee';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-//   id:number = 0
 
   constructor(private http:HttpClient) { 
     this.header.append("accept", "text/json");
     this.header.append("Access-Control-Allow-Origin", "*")
   }
 header: HttpHeaders = new HttpHeaders();
-  retrieveInfo() : Observable<Employee>{
-    return this.http.get("http://localhost:9000/revWorkforce/getInfo", 
+  retrieveInfo(id: number) : Observable<Employee>{
+    return this.http.get("http://localhost:9000/revWorkforce/employee/"+id, 
         {headers: this.header})
   }
   updateInfo(employee:Employee) : Observable<Employee> {
-    return this.http.post<Employee>("http://localhost:9000/revWorkforce/updateInfo", 
+    return this.http.patch<Employee>("http://localhost:9000/revWorkforce/employee/"+employee.id, 
     employee, {headers: this.header})
   }
 }
