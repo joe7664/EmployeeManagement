a12c9074266f0353ae6bc7dfdaffdf61505050fb
Joe <joe7663@gmail.com>
2023-04-18 17:02:40 -0400

    leave apply dates and holiday retrieval done

--- src/app/components/leave-apply/leave-apply.component.ts
+++ src/app/components/leave-apply/leave-apply.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { Employee } from 'src/app/models/Employee';
 import { Leave } from 'src/app/models/Leave';
 import { LeaveService } from 'src/app/services/leaves.service';
@@ -12,18 +12,18 @@ import { ProfileService } from 'src/app/services/profile.service';
 })
 export class LeaveApplyComponent {
   date: Date = new Date();
-  end: Date = new Date(this.date);
+  end: Date = new Date();
   tomorrow:string='';
   available:string='';
   leave: Leave={
-    startDate:undefined,
+    startDate:this.date,
     endDate:undefined,
     leaveType:'',
     status:'Submitted',
     notes:'n/a'
   };
   employee: Employee = {
-    leaveBalance:1,
+    leaveBalance:0,
     id:this.loginService.id
   };
 
@@ -35,9 +35,22 @@ export class LeaveApplyComponent {
     this.profileService.retrieveInfo(this.loginService.id).subscribe(json => this.employee = json);
     this.date.setDate(this.date.getDate()+1);
     this.tomorrow=this.date.toISOString().substring(0, 10);
+    this.leave.startDate=this.date;
+    console.log("full start date: ", this.leave.startDate);
+  }
+  onChange(changes:any){
+    console.log("selection changes: ", changes);
+    if(this.employee.leaveBalance!=undefined && this.leave.startDate!=undefined){
+      console.log("start date: ", this.leave.startDate);
+      this.end=new Date(this.leave.startDate);
+      this.end.setDate(new Date(this.leave.startDate).getDate()+this.employee.leaveBalance);
+      console.log("end date: ", this.end);
+      this.available=this.end.toISOString().substring(0, 10);
+      console.log("end date by available days: ", this.available);
+    }
   }
   submit(){
-    console.log(this.employee.leaveBalance)
+    console.log(this.employee.leaveBalance);
     this.leaveService.leaveRequest(this.leave, this.loginService.id).subscribe(json => console.log(json));
   }
 }
