d51dadbdff58993c4707b05ff52b31a5b97de9d7
Sadrach <sadrach@Jesuss-MacBook-Air.local>
2023-04-25 11:32:27 -0700

    Temp

--- src/app/pages/admin/leaves/leavesAdmin.component.ts
+++ src/app/pages/admin/leaves/leavesAdmin.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Leave } from 'src/app/models/Leave';
 import { AdminService } from 'src/app/services/admin.service';
 import { ManagerServiceService } from 'src/app/services/manager-service.service';
@@ -13,11 +13,17 @@ import { Employee } from 'src/app/models/Employee';
 export class LeavesAdminComponent implements OnChanges{
   @Input() leaves:Leave[] = []
   @Input() employee:Employee = {}
+  expandedElement:Leave = {}
   leaveColumns = ['name', 'startDate', 'endDate', 'status', 'notes','feedback', ]
-  constructor(private adminService: AdminService, private leaveService:LeaveService) {
+  constructor(private adminService: AdminService, private leaveService:LeaveService, private cd: ChangeDetectorRef) {
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes)
   }
+
+  toggleRow(element: Leave) {
+    this.expandedElement = element
+    this.cd.detectChanges();
+   }
 
 }
